d4ca0a852f805c2953641d3b29ab2b07d8091c6e
Vvivekvyas <vivekvyas2304@gmail.com>
2026-09-07 12:48:20 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void solve(vector<int>& nums,vector<int>& temp,vector<vector<int>>&ans,vector<bool>&vis){
+        if(temp.size() == nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+
+        for(int i=0 ; i<nums.size() ; i++){
+            if(!vis[i]){
+                vis[i] = true;
+                temp.push_back(nums[i]);
+                solve(nums,temp,ans,vis);
+                temp.pop_back();
+                vis[i] = false;
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int>temp;
+        vector<vector<int>>ans;
+        vector<bool>vis(nums.size(),false);
+        solve(nums,temp,ans,vis);
+        return ans;
+    }
+};
