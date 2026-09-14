a4a98b5616f53b6ee63bb63d91c684257c35c72d
Vvivekvyas <vivekvyas2304@gmail.com>
2026-09-14 10:23:40 +0530

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int solve(vector<int>& cost , int i, vector<int>& dp){
+        if(i == 0){
+           return cost[0];
+        }
+        if(i == 1){
+           return cost[1];
+        }
+
+        if(dp[i] != -1) return dp[i];
+
+        return dp[i] = cost[i] + min(solve(cost,i-1,dp),solve(cost,i-2,dp));
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int>dp(n,-1);
+        return min(solve(cost,n-1,dp),solve(cost,n-2,dp));
+    }
+};
